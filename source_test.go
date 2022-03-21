ea362ff138ff19eae7f1bdcf5ada1545d7e64550
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-03-21 15:27:00 +0100

    split source start context tests

--- source_test.go
+++ source_test.go
@@ -23,37 +23,49 @@ import (
 	"github.com/matryer/is"
 )
 
-func TestSourcePluginAdapter_Start_Context(t *testing.T) {
+func TestSourcePluginAdapter_Start_OpenContext(t *testing.T) {
 	is := is.New(t)
 	ctrl := gomock.NewController(t)
 	src := NewMockSource(ctrl)
 
+	srcPlugin := NewSourcePlugin(src).(*sourcePluginAdapter)
+
 	var gotCtx context.Context
-	src.EXPECT().
-		Open(gomock.Any(), Position(nil)).
+	src.EXPECT().Open(gomock.Any(), Position(nil)).
 		DoAndReturn(func(ctx context.Context, _ Position) error {
-			gotCtx = ctx // assign to gotCtx so it can be inspected by subtest
+			gotCtx = ctx // assign to gotCtx so it can be inspected
 			return ctx.Err()
-		}).
-		AnyTimes()
+		})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	_, err := srcPlugin.Start(ctx, cpluginv1.SourceStartRequest{})
+	is.NoErr(err)
+	is.NoErr(gotCtx.Err()) // expected context to be open
+
+	// even if we cancel the context afterwards, the context in Open should stay open
+	cancel()
+	is.NoErr(gotCtx.Err()) // expected context to be open
+
+}
+
+func TestSourcePluginAdapter_Start_ClosedContext(t *testing.T) {
+	is := is.New(t)
+	ctrl := gomock.NewController(t)
+	src := NewMockSource(ctrl)
 
 	srcPlugin := NewSourcePlugin(src).(*sourcePluginAdapter)
 
-	t.Run("open context", func(t *testing.T) {
-		ctx, cancel := context.WithCancel(context.Background())
-		_, err := srcPlugin.Start(ctx, cpluginv1.SourceStartRequest{})
-		is.NoErr(err)
-		is.NoErr(gotCtx.Err()) // expected context to be open
+	var gotCtx context.Context
+	src.EXPECT().Open(gomock.Any(), Position(nil)).
+		DoAndReturn(func(ctx context.Context, _ Position) error {
+			gotCtx = ctx // assign to gotCtx so it can be inspected
+			return ctx.Err()
+		})
 
-		// even if we cancel the context afterwards, the context in Open should stay open
-		cancel()
-		is.NoErr(gotCtx.Err()) // expected context to be open
-	})
-
-	t.Run("closed context", func(t *testing.T) {
-		ctx, cancel := context.WithCancel(context.Background())
-		cancel()
-		_, err := srcPlugin.Start(ctx, cpluginv1.SourceStartRequest{})
-		is.Equal(err, ctx.Err())
-	})
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	_, err := srcPlugin.Start(ctx, cpluginv1.SourceStartRequest{})
+	is.True(err != nil)
+	is.Equal(err, ctx.Err())
+	is.Equal(gotCtx.Err(), context.Canceled)
 }
