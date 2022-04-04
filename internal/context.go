d6e23e8213bb35cc591f5c2b24863d4373e17ea2
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-04-04 16:57:58 +0200

    go fmt

--- internal/context.go
+++ internal/context.go
@@ -22,11 +22,11 @@ import (
 // The following code is based on https://cs.opensource.google/go/x/tools/+/refs/tags/v0.1.10:internal/xcontext/xcontext.go
 //
 // Copyright (c) 2009 The Go Authors. All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-// 
+//
 //    * Redistributions of source code must retain the above copyright
 // notice, this list of conditions and the following disclaimer.
 //    * Redistributions in binary form must reproduce the above
@@ -36,7 +36,7 @@ import (
 //    * Neither the name of Google Inc. nor the names of its
 // contributors may be used to endorse or promote products derived from
 // this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
