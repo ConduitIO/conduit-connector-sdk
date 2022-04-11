4f930eff4dedc4e311f1d917e5acece4aa7a44de
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-04-11 17:34:35 +0200

    more tests

--- acceptance_testing.go
+++ acceptance_testing.go
@@ -15,6 +15,7 @@
 package sdk
 
 import (
+	"context"
 	"reflect"
 	"regexp"
 	"runtime"
@@ -34,7 +35,7 @@ import (
 //   }
 //
 func AcceptanceTest(t *testing.T, cfg AcceptanceTestConfig) {
-	run(t, cfg, testSpecifier_Specify_Success)
+	acceptanceTest{config: cfg}.Test(t)
 }
 
 type AcceptanceTestConfig struct {
@@ -50,45 +51,52 @@ type AcceptanceTestConfig struct {
 	DestinationConfig map[string]string
 }
 
-func run(t *testing.T, cfg AcceptanceTestConfig, test func(*testing.T, AcceptanceTestConfig)) {
-	name := runtime.FuncForPC(reflect.ValueOf(test).Pointer()).Name()
-	name = name[strings.LastIndex(name, ".")+1:]
-	t.Run(name, func(t *testing.T) { test(t, cfg) })
+type acceptanceTest struct {
+	config AcceptanceTestConfig
 }
 
-func testSpecifier_Specify_Success(t *testing.T, cfg AcceptanceTestConfig) {
+func (a acceptanceTest) Test(t *testing.T) {
+	a.run(t, a.testSpecifier_Specify_Success)
+	a.run(t, a.testSource_Configure_Success)
+	a.run(t, a.testSource_Configure_RequiredParams)
+}
+
+func (acceptanceTest) run(t *testing.T, test func(*testing.T)) {
+	name := runtime.FuncForPC(reflect.ValueOf(test).Pointer()).Name()
+	name = name[strings.LastIndex(name, ".")+1:]
+	t.Run(name, func(t *testing.T) { test(t) })
+}
+
+func (a acceptanceTest) testSpecifier_Specify_Success(t *testing.T) {
+	a.hasSpecFactory(t)
 	is := is.NewRelaxed(t) // allow multiple failures for this test
 
-	if cfg.SpecFactory == nil {
-		t.Fatalf("acceptance test config is missing the field SpecFactory")
-	}
-
-	specification := cfg.SpecFactory()
+	spec := a.config.SpecFactory()
 
 	// -- general ---------------------
 
-	is.True(specification.Name != "")                                    // Specification.Name is missing
-	is.True(strings.TrimSpace(specification.Name) == specification.Name) // Specification.Name starts or ends with whitespace
+	is.True(spec.Name != "")                           // Specification.Name is missing
+	is.True(strings.TrimSpace(spec.Name) == spec.Name) // Specification.Name starts or ends with whitespace
 
-	is.True(specification.Summary != "")                                       // Specification.Summary is missing
-	is.True(strings.TrimSpace(specification.Summary) == specification.Summary) // Specification.Summary starts or ends with whitespace
+	is.True(spec.Summary != "")                              // Specification.Summary is missing
+	is.True(strings.TrimSpace(spec.Summary) == spec.Summary) // Specification.Summary starts or ends with whitespace
 
-	is.True(specification.Description != "")                                           // Specification.Description is missing
-	is.True(strings.TrimSpace(specification.Description) == specification.Description) // Specification.Description starts or ends with whitespace
+	is.True(spec.Description != "")                                  // Specification.Description is missing
+	is.True(strings.TrimSpace(spec.Description) == spec.Description) // Specification.Description starts or ends with whitespace
 
-	is.True(specification.Version != "")                                       // Specification.Version is missing
-	is.True(strings.TrimSpace(specification.Version) == specification.Version) // Specification.Version starts or ends with whitespace
+	is.True(spec.Version != "")                              // Specification.Version is missing
+	is.True(strings.TrimSpace(spec.Version) == spec.Version) // Specification.Version starts or ends with whitespace
 
-	is.True(specification.Author != "")                                      // Specification.Author is missing
-	is.True(strings.TrimSpace(specification.Author) == specification.Author) // Specification.Author starts or ends with whitespace
+	is.True(spec.Author != "")                             // Specification.Author is missing
+	is.True(strings.TrimSpace(spec.Author) == spec.Author) // Specification.Author starts or ends with whitespace
 
 	// TODO connectors can also be only destinations or only sources
 	// TODO should we enforce that there is at least 1 parameter? what source wouldn't need any parameters?
-	is.True(specification.DestinationParams != nil)   // Specification.DestinationParams is missing
-	is.True(len(specification.DestinationParams) > 0) // Specification.DestinationParams is empty
+	is.True(spec.DestinationParams != nil)   // Specification.DestinationParams is missing
+	is.True(len(spec.DestinationParams) > 0) // Specification.DestinationParams is empty
 
-	is.True(specification.SourceParams != nil)   // Specification.SourceParams is missing
-	is.True(len(specification.SourceParams) > 0) // Specification.SourceParams is empty
+	is.True(spec.SourceParams != nil)   // Specification.SourceParams is missing
+	is.True(len(spec.SourceParams) > 0) // Specification.SourceParams is empty
 
 	// -- specifics -------------------
 
@@ -96,5 +104,65 @@ func testSpecifier_Specify_Success(t *testing.T, cfg AcceptanceTestConfig) {
 		`(-([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?` +
 		`(\+([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?`)
 
-	is.True(semverRegex.MatchString(specification.Version)) // Specification.Version is not a valid semantic version (vX.Y.Z)
+	is.True(semverRegex.MatchString(spec.Version)) // Specification.Version is not a valid semantic version (vX.Y.Z)
+}
+
+func (a acceptanceTest) testSource_Configure_Success(t *testing.T) {
+	a.hasSourceFactory(t)
+	is := is.New(t)
+	ctx := context.Background()
+
+	source := a.config.SourceFactory()
+	err := source.Configure(ctx, a.config.SourceConfig)
+	is.NoErr(err)
+}
+
+func (a acceptanceTest) testSource_Configure_RequiredParams(t *testing.T) {
+	a.hasSpecFactory(t)
+	a.hasSourceFactory(t)
+	is := is.New(t)
+	ctx := context.Background()
+
+	spec := a.config.SpecFactory()
+	for name, p := range spec.SourceParams {
+		if p.Required {
+			// removing the required parameter from the config should provoke an error
+			t.Run(name, func(t *testing.T) {
+				srcCfg := a.cloneConfig(a.config.SourceConfig)
+				delete(srcCfg, name)
+
+				is.Equal(len(srcCfg)+1, len(a.config.SourceConfig)) // source config does not contain required parameter, please check the test setup
+
+				source := a.config.SourceFactory()
+				err := source.Configure(ctx, srcCfg)
+				is.True(err != nil)
+			})
+		}
+	}
+}
+
+func (a acceptanceTest) hasSpecFactory(t *testing.T) {
+	if a.config.SpecFactory == nil {
+		t.Fatalf("acceptance test config is missing the field SpecFactory")
+	}
+}
+
+func (a acceptanceTest) hasSourceFactory(t *testing.T) {
+	if a.config.SourceFactory == nil {
+		t.Fatalf("acceptance test config is missing the field SourceFactory")
+	}
+}
+
+func (a acceptanceTest) hasDestinationFactory(t *testing.T) {
+	if a.config.DestinationFactory == nil {
+		t.Fatalf("acceptance test config is missing the field DestinationFactory")
+	}
+}
+
+func (a acceptanceTest) cloneConfig(orig map[string]string) map[string]string {
+	cloned := make(map[string]string, len(orig))
+	for k, v := range orig {
+		cloned[k] = v
+	}
+	return cloned
 }
