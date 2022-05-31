c3a3075d7dd8b5daf675db8dd4185b4c5f36ddf2
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-05-31 17:29:56 +0200

    implement holder utility struct

---
+++ internal/holder.go
@@ -0,0 +1,120 @@
+// Copyright © 2022 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+)
+
+// Holder holds a reference to a value. Multiple goroutines are able to put or
+// retrieve the value into/from the Holder, as well as wait for a certain value
+// to be put into the Holder.
+type Holder[T any] struct {
+	val      T
+	m        sync.RWMutex
+	listener chan T // a single listener, if needed we can expand to multiple in the future
+}
+
+// Put stores val in Holder and notifies the goroutine that called Await about
+// the new value, if such a goroutine exists.
+func (h *Holder[T]) Put(val T) {
+	h.m.Lock()
+	defer h.m.Unlock()
+
+	h.val = val
+	if h.listener != nil {
+		h.listener <- val
+	}
+}
+
+// Get returns the current value in Holder.
+func (h *Holder[T]) Get() T {
+	h.m.RLock()
+	defer h.m.RUnlock()
+
+	return h.val
+}
+
+// Await blocks and calls foundVal for every value that is put into the Holder.
+// Once foundVal returns true it stops blocking and returns nil. First call
+// foundVal will be with the current value stored in Holder. Await can only be
+// called by one goroutine at a time (we don't need anything more fancy right
+// now), if two goroutines call Await one will receive an error. If the context
+// gets cancelled before foundVal returns true, the function will return the
+// context error.
+func (h *Holder[T]) Await(ctx context.Context, foundVal func(val T) bool) error {
+	err := h.subscribe()
+	if err != nil {
+		// the only option subscribe produces an error is if it is called
+		// concurrently which is an invalid use case at the moment
+		return fmt.Errorf("invalid use of Holder.Await: %w", err)
+	}
+	defer h.unsubscribe()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case val := <-h.listener:
+			if foundVal(val) {
+				return nil
+			}
+		}
+	}
+}
+
+func (h *Holder[T]) subscribe() error {
+	h.m.Lock()
+	defer h.m.Unlock()
+
+	if h.listener != nil {
+		return errors.New("another goroutine is already subscribed to changes")
+	}
+
+	h.listener = make(chan T)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		// make sure the current value is sent to the listener
+		currentVal := h.val // copy val
+		wg.Done()           // signal to subscribe function it can unlock the mutex
+		h.listener <- currentVal
+	}()
+
+	// wait for goroutine to copy val
+	wg.Wait()
+
+	return nil
+}
+
+func (h *Holder[T]) unsubscribe() {
+	// drain channel and remove it
+	go func(in chan T) {
+		for range in {
+			// do nothing, just drain channel in case new values come in
+			// while we try to unsubscribe
+		}
+	}(h.listener)
+
+	h.m.Lock()
+	defer h.m.Unlock()
+
+	close(h.listener)
+	h.listener = nil
+}
---
+++ internal/holder_test.go
@@ -0,0 +1,156 @@
+// Copyright © 2022 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/matryer/is"
+)
+
+func TestHolder_GetEmptyValue(t *testing.T) {
+	is := is.New(t)
+
+	var h Holder[int]
+	got := h.Get()
+	is.Equal(0, got)
+}
+
+func TestHolder_GetEmptyPtr(t *testing.T) {
+	is := is.New(t)
+
+	var h Holder[*int]
+	got := h.Get()
+	is.Equal(nil, got)
+}
+
+func TestHolder_PutGetValue(t *testing.T) {
+	is := is.New(t)
+
+	var h Holder[int]
+	want := 123
+	h.Put(want)
+	got := h.Get()
+	is.Equal(want, got)
+}
+
+func TestHolder_PutGetPtr(t *testing.T) {
+	is := is.New(t)
+
+	var h Holder[*int]
+	want := 123
+	h.Put(&want)
+	got := h.Get()
+	is.Equal(&want, got)
+}
+
+func TestHolder_AwaitSuccess(t *testing.T) {
+	is := is.New(t)
+
+	var h Holder[int]
+
+	putValue := make(chan int)
+	defer close(putValue)
+	go func() {
+		for val := range putValue {
+			h.Put(val)
+		}
+	}()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	i := 0
+	err := h.Await(ctx, func(val int) bool {
+		i++
+		switch i {
+		case 1:
+			is.Equal(0, val) // expected first value to be 0
+			putValue <- 123  // put next value
+			return false     // not the value we are looking for
+		case 2:
+			is.Equal(123, val)
+			putValue <- 555 // put next value
+			return false    // not the value we are looking for
+		case 3:
+			is.Equal(555, val)
+			return true // that's what we were looking for
+		default:
+			is.Fail() // unexpected value for i
+			return false
+		}
+	})
+	is.NoErr(err)
+	is.Equal(3, i)
+
+	got := h.Get()
+	is.Equal(555, got)
+
+	// we can still put more values into the holder
+	h.Put(666)
+	got = h.Get()
+	is.Equal(666, got)
+}
+
+func TestHolder_AwaitContextCancel(t *testing.T) {
+	is := is.New(t)
+
+	var h Holder[int]
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*10)
+	defer cancel()
+
+	i := 0
+	err := h.Await(ctx, func(val int) bool {
+		i++
+		is.Equal(0, val)
+		return false
+	})
+
+	is.Equal(ctx.Err(), err)
+	is.Equal(1, i)
+}
+
+func TestHolder_AwaitMultiple(t *testing.T) {
+	is := is.New(t)
+
+	var h Holder[int]
+
+	var wg1, wg2, wg3 sync.WaitGroup
+	wg1.Add(1)
+	wg2.Add(1)
+	wg3.Add(1)
+	go func() {
+		defer wg3.Done()
+		err := h.Await(context.Background(), func(val int) bool {
+			wg1.Done()
+			wg2.Wait() // wait until test says it's ok to return
+			return true
+		})
+		is.NoErr(err)
+	}()
+
+	wg1.Wait() // wait for Await to actually run
+
+	// try to run await a second time
+	err := h.Await(context.Background(), func(val int) bool { return false })
+	is.True(err != nil) // expected an error from second Await call
+
+	wg2.Done() // signal to first Await call to return
+	wg3.Wait() // wait for goroutine to stop running
+}
