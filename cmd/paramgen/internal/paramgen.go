60a00695e04f0439a5ef783d53f538c29cf1de04
Haris Osmanagić <haris@meroxa.io>
2024-04-15 17:23:39 +0200

    Include package names in error (#118)

--- cmd/paramgen/internal/paramgen.go
+++ cmd/paramgen/internal/paramgen.go
@@ -29,6 +29,7 @@ import (
 	"unicode"
 
 	sdk "github.com/conduitio/conduit-connector-sdk"
+	"golang.org/x/exp/maps"
 )
 
 const (
@@ -123,7 +124,7 @@ func parsePackage(path string) (*ast.Package, error) {
 		return nil, fmt.Errorf("no source-code package in directory %s", path)
 	}
 	if len(pkgs) > 1 {
-		return nil, fmt.Errorf("multiple packages in directory %s", path)
+		return nil, fmt.Errorf("multiple packages %v in directory %s", maps.Keys(pkgs), path)
 	}
 	for _, v := range pkgs {
 		return v, nil // return first package
