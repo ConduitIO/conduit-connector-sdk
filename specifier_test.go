0e69f6f1fe63a0f308dd8bb7e8f8031aa3a5dc1d
Lyubo Kamenov <lyubo@meroxa.io>
2024-09-13 18:45:57 -0400

    Assign source and dst params to spec (#185)
    * Assign source and dst params to spec

--- specifier_test.go
+++ specifier_test.go
@@ -18,7 +18,9 @@ import (
 	"context"
 	"testing"
 
+	"github.com/conduitio/conduit-commons/config"
 	"github.com/conduitio/conduit-connector-protocol/pconnector"
+	"github.com/google/go-cmp/cmp"
 	"github.com/matryer/is"
 )
 
@@ -36,4 +38,126 @@ func TestSpecifier_NilDestination(t *testing.T) {
 	p := NewSpecifierPlugin(Specification{}, UnimplementedSource{}, nil)
 	_, err := p.Specify(context.Background(), pconnector.SpecifierSpecifyRequest{})
 	is.NoErr(err)
+}
+
+func TestSpecifier_Connector(t *testing.T) {
+	is := is.New(t)
+
+	c := Connector{
+		NewSpecification: testConnSpec,
+		NewSource:        newTestSpecSource,
+		NewDestination:   newTestSpecDestination,
+	}
+
+	s := NewSpecifierPlugin(c.NewSpecification(), c.NewSource(), c.NewDestination())
+	got, err := s.Specify(context.Background(), pconnector.SpecifierSpecifyRequest{})
+
+	want := pconnector.SpecifierSpecifyResponse{
+		Specification: pconnector.Specification{
+			Name:        "testSpecConn",
+			Summary:     "summary of spec conn",
+			Description: "desc of spec conn",
+			Version:     "(devel+1)",
+			Author:      "sdk-max",
+			SourceParams: map[string]config.Parameter{
+				"srcParam1": {
+					Default:     "",
+					Description: "source param 1",
+					Type:        config.ParameterTypeString,
+					Validations: []config.Validation{
+						config.ValidationRequired{},
+					},
+				},
+				"srcParam2": {
+					Default:     "set",
+					Description: "source param 2",
+					Type:        config.ParameterTypeString,
+					Validations: []config.Validation{},
+				},
+			},
+			DestinationParams: map[string]config.Parameter{
+				"destParam1": {
+					Default:     "",
+					Description: "dest param 1",
+					Type:        config.ParameterTypeString,
+					Validations: []config.Validation{
+						config.ValidationRequired{},
+					},
+				},
+				"destParam2": {
+					Default:     "unset",
+					Description: "dest param 2",
+					Type:        config.ParameterTypeString,
+					Validations: []config.Validation{},
+				},
+			},
+		},
+	}
+
+	is.NoErr(err)
+	is.Equal("", cmp.Diff(want, got))
+}
+
+func testConnSpec() Specification {
+	return Specification{
+		Name:        "testSpecConn",
+		Summary:     "summary of spec conn",
+		Description: "desc of spec conn",
+		Version:     "(devel+1)",
+		Author:      "sdk-max",
+	}
+}
+
+type testSpecSource struct {
+	UnimplementedSource
+}
+
+func newTestSpecSource() Source {
+	return &testSpecSource{}
+}
+
+func (s *testSpecSource) Parameters() config.Parameters {
+	return map[string]config.Parameter{
+		"srcParam1": {
+			Default:     "",
+			Description: "source param 1",
+			Type:        config.ParameterTypeString,
+			Validations: []config.Validation{
+				config.ValidationRequired{},
+			},
+		},
+		"srcParam2": {
+			Default:     "set",
+			Description: "source param 2",
+			Type:        config.ParameterTypeString,
+			Validations: []config.Validation{},
+		},
+	}
+}
+
+type testSpecDestination struct {
+	UnimplementedDestination
+}
+
+func newTestSpecDestination() Destination {
+	return &testSpecDestination{}
+}
+
+func (d *testSpecDestination) Parameters() config.Parameters {
+	return map[string]config.Parameter{
+		"destParam1": {
+			Default:     "",
+			Description: "dest param 1",
+			Type:        config.ParameterTypeString,
+			Validations: []config.Validation{
+				config.ValidationRequired{},
+			},
+		},
+		"destParam2": {
+			Default:     "unset",
+			Description: "dest param 2",
+			Type:        config.ParameterTypeString,
+			Validations: []config.Validation{},
+		},
+	}
 }
